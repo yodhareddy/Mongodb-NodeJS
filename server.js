d5c2195b72d2ef9cddad98a0684e3bcdf984a5f2
yodhareddy <yodhareddy@gmail.com>
2018-08-13 11:28:34 -0500

    CRUD operations are included

--- server.js
+++ server.js
@@ -9,12 +9,49 @@ mongoose.connect('mongodb://localhost/test1', function (err) {
 
 //Student is the Model
 var newStudent = new Student({
-    firstName: 'Chandler',
-    lastName: 'Bing'
+    firstName: 'Rachel',
+    lastName: 'Green'
 });
 
-//Save the model into the database
+//Below are the CRUD operations:
+
+//CREATE the model into the database
 newStudent.save(function(err){
     if(err) throw err;
-        console.log("Student inserted");
+       console.log("Student inserted");
+ });
+
+
+//READ all rows
+Student.find(function(err,students){
+    if(err) throw err;
+    console.log(students);
+});
+
+//READ with conditions
+
+//findOne function returns only one row.
+Student.find({lastName:'Geller',firstName:'Monica'},function(err,student){
+    if(err) throw err;
+    console.log(student);
+
+});
+
+//READ using ID and UPDATE the value.
+//We can also use .where().exec() function to give more constraints.
+//findByIdAndUpdate()
+Student.findById('5b71a4b670f31794ae4cc165',function(err,student){
+
+    if(err) throw err;
+   student.lastName = 'Geller';
+   student.save(function(e){
+       if(e) throw e;
+       console.log("Student updated");
+    });
+});
+
+//DELETE
+//.findByIDAndRemove() can also be used.
+Student.remove({lastName:'Bing'},function(e){
+    if(e) throw e;
 });
